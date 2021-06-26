ad07b31bfc9345a666cfa280e0a0a82a325e3170
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2021-06-26 18:09:56 +1000

    VIS-923: use dragThing from htmlBuildUtils

--- theSrc/test/lib/sankeyTreePage.js
+++ theSrc/test/lib/sankeyTreePage.js
@@ -1,3 +1,5 @@
+const { snapshotTesting: { interactions: { dragThing } } } = require('rhtmlBuildUtils')
+
 class SankeyTreePage {
   constructor (page) {
     this.page = page
@@ -10,29 +12,9 @@ class SankeyTreePage {
 
   async clickNodeRect (labelId) { return this.page.click(this.nodeRectSelector(labelId)) }
 
-  // duplicated in rhtmlMoonplot
-  async dragThing (selector, xMovement, yMovement) {
-    const coords = await this.getCoords(selector)
-    const steps = Math.floor(Math.max(Math.abs(xMovement), Math.abs(yMovement)) / 2)
-    await this.page.hover(selector)
-    await this.page.mouse.down()
-    await this.page.mouse.move(parseFloat(coords.left + xMovement), parseFloat(coords.top + yMovement), { steps })
-    return this.page.mouse.up()
-  }
-
-  // duplicated in rhtmlMoonplot
-  async getCoords (selector) {
-    const element = await this.page.$(selector)
-    const rect = await this.page.evaluate(element => {
-      const { top, left, bottom, right } = element.getBoundingClientRect()
-      return { top, left, bottom, right }
-    }, element)
-    return rect
-  }
-
   async dragView (xMovement, yMovement) {
     const selector = this.canvasSelector()
-    return this.dragThing(selector, xMovement, yMovement)
+    return dragThing(this.page, selector, xMovement, yMovement)
   }
 }
 
